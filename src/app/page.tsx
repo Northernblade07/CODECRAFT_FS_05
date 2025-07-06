be12cc7844165abd558ff35a8d41410104a08cd2
Prahant Bhandari <prashantbhandaricr07@gmail.com>
2025-07-06 17:31:43 +0530

    set up clerk and shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
-
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ModeToggle";
 export default function Home() {
   return (
-    <div>
-      <h1>hello home page</h1>
+    <div className="m-4">
+             <SignedOut>
+              <SignInButton mode="modal" />
+              <SignUpButton mode="modal">
+                <Button>
+                  Sign in
+                </Button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+<ModeToggle/>
+            <Button variant={'secondary'}>Click me</Button>
     </div>
   );
 }
---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import * as React from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export default function ModeToggle() {
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      {resolvedTheme === "dark" ? (
+        <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      {/* <span className="sr-only"></span> */}
+    </Button>
+  );
+}
