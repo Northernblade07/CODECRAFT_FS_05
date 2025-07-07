d9c1f61717352ae1340f1290a48ef64689401caf
Prahant Bhandari <prashantbhandaricr07@gmail.com>
2025-07-07 16:29:41 +0530

    finish  auth and db sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,9 @@
-import Image from "next/image";
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
-             <SignedOut>
-              <SignInButton mode="modal" />
-              <SignUpButton mode="modal">
-                <Button>
-                  Sign in
-                </Button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-
-            {/* <Button variant={'secondary'}>Click me</Button> */}
+             
     </div>
   );
 }
